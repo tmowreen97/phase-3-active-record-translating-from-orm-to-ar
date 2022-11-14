83c6dd0dd52d21bde7a9ed90c9116fb35f62b86a
Tasreen Mowreen <tmowreen97@gmail.com>
2022-11-14 18:34:09 -0500

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -10,3 +10,21 @@ ActiveRecord::Base.establish_connection(
   adapter: "sqlite3",
   database: "./db/test.sqlite3"
 )
+
+sql = <<-SQL
+  CREATE TABLE IF NOT EXISTS dogs (
+    id INTEGER PRIMARY KEY,
+    name TEXT
+  )
+SQL
+ActiveRecord::Base.connection.execute(sql)
+
+# class Dog < ActiveRecord::Base
+# end
+
+
+
+
+# # Log SQL output to the terminal
+# ActiveRecord::Base.logger = Logger.new(STDOUT)
+
